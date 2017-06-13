1a47931f4752c37b91bfcdf0da5e9cd98c59be52
Simon Fels <simon.fels@canonical.com>
2017-06-13 10:29:08 +0200

    Don't use Ubuntu proposed archive on Raspbian

--- tests/lib/pkgdb.sh
+++ tests/lib/pkgdb.sh
@@ -219,7 +219,9 @@ distro_install_build_snapd(){
     if [ "$SRU_VALIDATION" = "1" ]; then
         apt install -y snapd
         cp /etc/apt/sources.list sources.list.back
-        echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -c -s)-proposed restricted main multiverse universe" | tee /etc/apt/sources.list -a
+        if [[ "$SPREAD_SYSTEM" = ubuntu-* ]]; then
+            echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -c -s)-proposed restricted main multiverse universe" | tee /etc/apt/sources.list -a
+        fi
         apt update
         apt install -y --only-upgrade snapd
         mv sources.list.back /etc/apt/sources.list
